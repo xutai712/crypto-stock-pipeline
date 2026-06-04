07500b994c1f6b3fa8b228d89031de4c75684343
Mutai <test@Ms-MacBook-Air.local>
2026-06-04 11:28:46 -0500

    Fixed fetch stocks & added load file

--- pipeline/fetch_stocks.py
+++ pipeline/fetch_stocks.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import os
 import time
 import requests
@@ -9,24 +10,18 @@ api_key = os.getenv("ALPHA_VANTAGE_API_KEY")
 stocks = ["AAPL", "GOOGL", "AMZN", "MSFT"]
 url = "https://www.alphavantage.co/query"
 
-for stock in stocks:
+def fetch_stock_row(stock):
     params = {
-    "function": "TIME_SERIES_DAILY",  # Changed to a free endpoint
-    "symbol": stock,
-    "apikey": api_key
-}
+        "function": "TIME_SERIES_DAILY",  # Changed to a free endpoint
+        "symbol": stock,
+        "apikey": api_key
+    }
     response = requests.get(url, params=params)
     data = response.json()
     time_series = data.get("Time Series (Daily)", {})
+    latest_day = list(time_series.keys())[0]
+    latest_data = time_series[latest_day]
+    print(latest_data)
+
+
     
-    print(f"\n--- {stock} ---")
-    if time_series:
-        latest_day = list(time_series.keys())[0]
-        latest_data = time_series[latest_day]
-        print(f"Latest Day: {latest_day}")
-        print(f"Latest Price: {latest_data['4. close']}")
-        print(f"Latest Volume: {latest_data['5. volume']}")
-    else:
-        print("No data. Raw response:")
-        print(data)
-    time.sleep(15) #API has a 5 calls per minute limit
---
+++ pipeline/load.py
@@ -0,0 +1,47 @@
+from db_connect import connect
+from fetch_crypto import fetch_coin_row
+from fetch_crypto import coins
+from fetch_stocks import fetch_stock_row
+from fetch_stocks import stocks
+
+
+def load_data():
+    conn = connect()
+    cursor = conn.cursor()
+
+    # Load stock data
+    for stock in stocks:
+        row = fetch_stock_row(stock)
+        cursor.execute("""
+            INSERT INTO Prices (ticker, price_date, open_price, high_price, low_price, close_price, volume)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+            ON DUPLICATE KEY UPDATE
+                open_price = VALUES(open_price),
+                high_price = VALUES(high_price),
+                low_price = VALUES(low_price),
+                close_price = VALUES(close_price),
+                volume = VALUES(volume) 
+        """, (row["ticker"], row["price_date"], row["open_price"], row["high_price"], row["low_price"], row["close_price"], row["volume"]))
+            
+    conn.commit()
+
+    # Load crypto data
+    for coin_id, ticker in coins.items():
+        row = fetch_coin_row(coin_id, ticker)
+        cursor.execute("""
+            INSERT INTO Prices (ticker, price_date, open_price, high_price, low_price, close_price, volume)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+            ON DUPLICATE KEY UPDATE
+                open_price = VALUES(open_price),
+                high_price = VALUES(high_price),
+                low_price = VALUES(low_price),
+                close_price = VALUES(close_price),
+                volume = VALUES(volume) 
+        """, (row["ticker"], row["price_date"], row["open_price"], row["high_price"], row["low_price"], row["close_price"], row["volume"])) 
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    load_data()
