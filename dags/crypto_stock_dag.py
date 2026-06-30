73058958a8139e64327a9f82cd0f1fb4e3c9ef5b
Mutai <test@Ms-MacBook-Air.local>
2026-06-30 16:44:11 -0500

    Add inline comment clarifying DAG schedule timezone

--- dags/crypto_stock_dag.py
+++ dags/crypto_stock_dag.py
@@ -9,7 +9,7 @@ from load import load_data
 with DAG(
     dag_id = "crypto_stock_pipeline",
     start_date = datetime(2026, 6, 1),
-    schedule = "0 11 * * *",
+    schedule = "0 11 * * *", #Run daily at 11am UTC or 6am CST
     catchup = False,
 ) as dag:
     load_prices = PythonOperator(
